c405bc60989c2af90fe88f5697e4b066c69e5260
Andrew <andrewborg86@gmail.com>
2021-10-11 16:22:22 +1100

    setting up functions, tests and basic interface

---
+++ index.js
@@ -0,0 +1,18 @@
+/**
+ * Given a string will convert characters into more code.
+ * @param {string} string
+ * @return {string}
+ */
+export const engToMorse = (string) => {
+
+}
+
+/**
+ * Given morse code will convert the code into a string of characters.
+ * @param {string} morse
+ * @return {string}
+ */
+export const morseToEng = (morse) => {
+
+}
+
---
+++ index.test.js
@@ -0,0 +1,59 @@
+describe('engToMorse', function () {
+  it('should return an empty string when given an empty string', function () {
+
+  });
+  it('should only accept strings as well as numbers', function () {
+
+  });
+  it('should handle spaces', function () {
+
+  });
+  it('should handle multiple characters', function () {
+
+  });
+  it('should handle special characters', function () {
+
+  });
+  it('should handle single characters', function () {
+
+  });
+  it('should handle numbers in a string', function () {
+
+  });
+  it('should handle periods .', function () {
+
+  });
+  it('should handle lower case characters', function () {
+
+  });
+  it('should handle uppercase characters', function () {
+
+  });
+  it('should handle multiple sentences', function () {
+
+  });
+});
+
+describe('morseToEng', function () {
+  it('should only accept 4 characters . - | and spaces.', function () {
+    
+  });
+  it('should return an empty string when given an empty string', function () {
+
+  });
+  it('should handle spaces', function () {
+
+  });
+  it('should handle multiple characters', function () {
+
+  });
+  it('should handle single characters', function () {
+
+  });
+  it('should handle multiple sentences', function () {
+
+  });
+  it('should not accept unknown alphabets', function () {
+
+  });
+});
